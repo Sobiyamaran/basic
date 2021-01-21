61c1ba9e7ea08bf4c95f79e4c5a3d85a643211bd
Sobiyamaran <77062277+Sobiyamaran@users.noreply.github.com>
2021-01-21 16:56:29 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+     int  i,j,n;
+
+     printf("enter the no of rows");
+     scanf("%d",&n);
+     printf("\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+          if(i==j||j==n-i+1)
+              printf("*");
+          else
+              printf(" ");
+
+
+        }
+        printf("\n");
+
+    }
+}
---
+++ pattern2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+     int  i,j,n;
+
+     printf("enter the no of rows");
+     scanf("%d",&n);
+     printf("\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+          if(i==1||j==n||i==n||j==1)
+              printf(" *");
+          else
+              printf("  ");
+
+        }
+        printf("\n");
+
+    }
+}
